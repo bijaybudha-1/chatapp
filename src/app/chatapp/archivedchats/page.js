77fc8fd0d8adbe79370cfb28ef8d7642fd27b965
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2025-01-30 15:28:29 +0545

    Setup all and needed route

---
+++ src/app/chatapp/archivedchats/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArchivedChats = () => {
+  return (
+    <div>ArchivedChats</div>
+  )
+}
+
+export default ArchivedChats
---
+++ src/app/chatapp/lockchats/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LockChats = () => {
+  return (
+    <div>LockChats</div>
+  )
+}
+
+export default LockChats
